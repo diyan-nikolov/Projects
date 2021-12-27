efe632b0de6d10244e7965d99d3710e6874b7fd5
Diyan Nikolov <78504231+diyan-nikolov@users.noreply.github.com>
2021-12-26 23:12:12 -0500

    Update SimpleStortage.sol

--- SimpleStortage.sol
+++ SimpleStortage.sol
@@ -1,16 +1,33 @@
+// SPDX-License-Identifier: MIT
 
-
-pragma solidity >= 0.6.0 < 0.9.0;
+pragma solidity ^0.6.0;
 
 contract SimpleStorage {
 
     // this will get initialized to 0
     uint256 public favoriteNumber;
+    bool favoriteBool;
+
+    // struct are ways to define create new types objects in Solidity 
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    } 
+    //creating a list of objects (array)
+    People[] public people;
+    mapping(string => uint256) public nameToFavoriteNumber;
 
     function store (uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
 
-    function retrieve() public view returns(uint256)
-    
+    //view, pure (does computations)
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    } 
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }   
 }
