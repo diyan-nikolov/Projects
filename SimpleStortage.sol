cb722e2c8092dceae19eb426aa9dabbbb233a346
Diyan Nikolov <78504231+diyan-nikolov@users.noreply.github.com>
2022-01-13 06:04:33 -0500

    Update SimpleStortage.sol

--- SimpleStortage.sol
+++ SimpleStortage.sol
@@ -29,5 +29,5 @@ contract SimpleStorage {
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
         people.push(People(_favoriteNumber, _name));
         nameToFavoriteNumber[_name] = _favoriteNumber;
-    }   
+    }
 }
